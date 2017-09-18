f9e5d3e257431e250eabaa8879ad8db2f59819ea
Amandeep Sekhon <amandeepsekhon@Amandeeps-MacBook-Pro.local>
2017-09-17 22:30:20 -0400

    Amandeep here

---
+++ mapd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mapd
+//
+//  Created by Amandeep Sekhon on 2017-09-17.
+//  Copyright © 2017 Amandeep Sekhon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
